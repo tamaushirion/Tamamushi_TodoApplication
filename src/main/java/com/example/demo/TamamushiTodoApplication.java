52a2f711e001f2a561af7a6934cfe87b9f390cb6
rionTamamushi <>
2024-11-22 19:06:59 +0900

    First Commit

---
+++ src/main/java/com/example/demo/TamamushiTodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamamushiTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamamushiTodoApplication.class, args);
+	}
+
+}
